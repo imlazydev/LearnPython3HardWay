037122f5f9985e48e755523d733bac750e314342
Mayuran <mayu.dev@outlook.com>
2018-02-22 17:07:37 +0530

    Completed Ex40

---
+++ Ex40-2.py
@@ -0,0 +1,17 @@
+        
+from Ex40 import *
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+happy_bday = Song([ "Happy birthday to you",
+                    "I don't want to get sued",
+                    "So I'll stop right there"])
+
+bulls_on_parade = Song( ["They rally aroud the family",
+                        "With pocket full of shells"])
+
+
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
---
+++ Ex40.py
@@ -0,0 +1,30 @@
+"""Exercise 40 - Modules, Classes and Objects"""
+
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now thousand years between"
+
+    def apple(self):
+        print("I'm apples")
+
+lyrics =''
+
+class Song(object):
+    def __init__(self,lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_bday = Song([ "Happy birthday to you",
+                    "I don't want to get sued",
+                    "So I'll stop right there"])
+
+bulls_on_parade = Song( ["They rally aroud the family",
+                        "With pocket full of shells"])
+
+
+#happy_bday.sing_me_a_song()
+#bulls_on_parade.sing_me_a_song()
